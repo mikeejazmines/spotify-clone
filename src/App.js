b82d6a5e5e32118042aa5f6d6d32fd8c637a17e5
Mikee Jazmines <mikee.jazmines@gmail.com>
2020-08-24 21:30:33 +0800

    Fix login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Spotify clone</h1>
+    <div className="app">
+      \{/* logo */}
+      {/* login */}
+      <Login />
+      {/*  */}
     </div>
   );
 }
