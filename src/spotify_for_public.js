76b92a469264fef9a83ed4cfa6036aff41a90cd0
Mikee Jazmines <mikee.jazmines@gmail.com>
2020-08-24 21:48:39 +0800

    Add spotify configurations

---
+++ src/spotify_for_public.js
@@ -0,0 +1,28 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "YOUR-CLIENT-ID-HERE"; // Removed my client id. Go to spotify developers to create your own
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+
+export const loginURL = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
