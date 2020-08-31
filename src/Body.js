ff740e35b4d200c56187dfe50ed08faf943e57ec
Mikee Jazmines <mikee.jazmines@gmail.com>
2020-08-31 20:04:32 +0800

    Body

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,14 @@ function Body({ spotify }) {
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body__info">
+        <img src="https://newjams-images.scdn.co/v2/discover-weekly/pOdnPTRma6KZzKPNRN9oUA==/bmVuZW5lbmVuZW5lbmVuZQ==/default" alt=""/>
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>description</p>
+        </div>
+      </div>
     </div>
   );
 }
